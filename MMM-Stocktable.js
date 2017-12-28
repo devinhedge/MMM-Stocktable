10a328ce659729371bb26fde8a8500952c494ef4
Devin Hedge <devin@devinhedge.com>
2017-12-28 16:21:26 +0000

    Updated to basic calls and function. Still broken so don't get excited.

--- MMM-Stocktable.js
+++ MMM-Stocktable.js
@@ -1,11 +1,11 @@
 // MMM-Stocktable.js
 
-Module.register("MMM-Stocktable",{
+Module.register("MMM-Stocktable", {
     // Default module config.
     defaults: {
         updateInterval: 60000,
         fadeSpeed: 3000,
-        stocks: ["GOOGL","YHOO"],
+        stocks: ["GOOGL", "YHOO"],
         currency: "usd",
         baseURL: "https://www.alphavantage.co/",
         apikey: "", //Get an API Key and override in the MagicMirror config.js
@@ -13,17 +13,89 @@ Module.register("MMM-Stocktable",{
         view: "stocks", // Override in the MagicMirror config.js 
                         // Valid values are: ["stocks","bonds","currency","index","gainloss","volume"]
         header: "Market Pulse",
-        text: "Hello, Devin! This isn't working."
+        content: "Hello, Devin! This isn't working."
     },
 
+    // Define start sequence.
+    start: function() {
+        Log.info(this.name + ' Start function called.');
+
+        this.loaded = false;
+
+        switch (this.config.view) {
+        case "stocks":
+            Log.info(this.name + ': Starting #DBH stocks node_helper.');
+            if (this.config.stocks) {
+                this.stocks = this.config.stocks;
+            }
+            this.sendSocketNotification("ADD_STOCKS", this.stocks);
+            break;
+        default:
+            Log.info('The module: ' + this.name + ' is missing configuration information.');
+            this.config.content = 'Check your configuration!';
+            return;
+        }
+    },
     // Override dom generator.
     getDom: function() {
+        this.content = this.config.content;
+        //Call the correct view based on the 'view' module property.
+        switch (this.config.view) {
+        case "stocks":
+            console.info(this.name + ': Calling stocks view.');
+            this.content = generateStocksView;
+            break;
+        case "bonds":
+            this.content = 'Calling bonds view.';
+            console.info(this.name + ': ' + this.content);
+            break;
+        case "currency":
+            this.content = 'Calliing currency view.';
+            console.info(this.content);
+            break;
+        case "index":
+            this.content = 'Calling index view.';
+            console.info(this.content);
+            break;
+        case "gainloss":
+            this.content = 'Calling gainloss view.';
+            console.info(this.content);
+            break;
+        case "volume":
+            this.content = 'Calling volume view.';
+            console.log(this.content);
+            break;
+        default:
+            this.content = 'Sorry, That view, ' + this.config.view + ', is invalid.';
+            console.error(this.content);
+        }
+        // Wrap the view content in an html div statement.
         var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.text;
+        wrapper.innerHTML = this.content;
         return wrapper;
-    }
+    },
     // Override header generator
-//    getHeader: function() {
-//        return this.data.header + ' - ' + this.data.view;
+    getHeader: function() {
+        return this.data.header + ' - ' + this.config.view;
+    },
+    generateStocksView: function() {
+        Log.log(this.name + ': generateStocksView called.');
+        var contentStocks = 'Dummy Stock data from generateStocksView().';
+        return contentStocks;
+    }
+//    generateBondsView function() {
+//        return this.content;
+//    },
+//    generateCurrencyView function() {
+//        return this.content;
+//    },
+//    generateIndexView function() {
+//        return this.content;
+//    },
+//    generateGainlossView function() {
+//        return this.content;
+//    },
+//    generateVolumeView function() {
+//        return this.content;
 //    }
 });
--- node_helper.js
+++ node_helper.js
@@ -12,5 +12,17 @@ module.exports = NodeHelper.create({
     start: function() {
         var events = [];
         console.log("Starting node helper for: " + this.name);
+    },
+    // Subclass socketNotificationReceived received.
+    socketNotificationReceived: function(notification, payload) {
+        switch (notification) {
+        case 'ADD_STOCKS':
+            this.content = this.name + ': Called stocks SETTER';
+            console.log(this.content);
+            break;
+        default:
+            console.error(this.name + ': No Stocks Configure. Please configure them in MagicMirror config.js.');
+        }
+        return;
     }
 });
